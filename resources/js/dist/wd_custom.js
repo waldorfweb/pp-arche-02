fe3d54e82f8d5da4442c53a2f43c205e3cc1636c
waldorfweb <alex@waldorfshop.eu>
2021-07-09 09:40:17 +0200

    mini css init 03

--- resources/js/dist/wd_custom.js
+++ resources/js/dist/wd_custom.js
@@ -32,11 +32,10 @@ $(function() {
         $('.textrow_color01').wrap('<div class="widget-full-width textrow_color01_wrapper"></div>');
 
 
-        $("body").removeClass("fixed");
         $(window).on("scroll touchmove", function()
             {
                 //$("body").addClass("touched-fixed");
-                if ($(document).scrollTop() <= $("body").position().top - 140 )
+                if ($(document).scrollTop() <= $("body").position().top - 0 )
                 {
                     //$(".et_toggle_fullscreen_menu").addClass("showplus");
                     console.log('add fixed class');
@@ -50,7 +49,8 @@ $(function() {
                 }
     
             });
-
+            
+        $("body").removeClass("fixed");
         //remove canonical tag
         //$(".cano").removeClass("cano");
     $(".search-input ").attr("placeholder", "Suche");
